196fda1d8c3699b7b619c734377e31eb9f67febc
Laide Endeley <endeleylaide@gmail.com>
2023-04-27 02:13:29 -0700

    Updated README.md and main.h; Added and compiled 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: the number of times the character _ should be printed.
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		_putchar(95);
+		a++;
+	}
+
+	_putchar('\n');
+}
