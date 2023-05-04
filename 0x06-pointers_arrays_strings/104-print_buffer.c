cf16348cbd013aca483058536605124712683cf6
Laide Endeley <endeleylaide@gmail.com>
2023-05-04 12:15:15 -0700

    Updated and compiled 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,35 +12,38 @@ void print_buffer(char *b, int size)
 {
 	int by, i;
 
-	for (by = 0; by < size; by += 10)
-	{
-		printf("%08x: ", by);
-
-		for (i = 0; i < 10; i++)
-		{
-			if ((i + by) >= size)
-				printf(" ");
-			else
-				printf("%02x", *(b + i + by));
-			if ((i % 2) != 0 && i != 0)
-				printf(" ");
-		}
-
-		for (i = 0; i < 10; i++)
-		{
-			if ((i + by) >= size)
-				break;
-			else if (*(b + i + by) >= 31 &&
-					*(b + i + by) <= 126)
-				printf("%c", *(b + i + by));
-			else
-				printf(".");
-		}
-		if (by >= size)
-			continue;
-
-		printf("\n");
-	}
 	if (size <= 0)
 		printf("\n");
+	else
+	{
+		for (by = 0; by < size; by += 10)
+		{
+			printf("%08x: ", by);
+
+			for (i = 0; i < 10; i++)
+			{
+				if ((i + by) >= size)
+					printf(" ");
+				else
+					printf("%02x", *(b + i + by));
+				if ((i % 2) != 0 && i != 0)
+					printf(" ");
+			}
+
+			for (i = 0; i < 10; i++)
+			{
+				if ((i + by) >= size)
+					break;
+				else if (*(b + i + by) >= 31 &&
+						*(b + i + by) <= 126)
+					printf("%c", *(b + i + by));
+				else
+					printf(".");
+			}
+			if (by >= size)
+				continue;
+
+			printf("\n");
+		}
+	}
 }
