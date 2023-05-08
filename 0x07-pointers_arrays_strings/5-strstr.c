388635572fc1985e3de6a5615076520024616e5f
Laide Endeley <endeleylaide@gmail.com>
2023-05-08 08:43:48 -0700

    Updated and compiled 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * compare - checks if two strings are the same
- * @a: first string to be checked
- * @b: second string to be checked
- *
- * Return: true if strings are the same
- * false if strings are not the same
- */
-int compare(char *a, char *b)
-{
-	while (*a && *b)
-	{
-		if (*a != *b)
-		{
-			return (0);
-		}
-		a++;
-		b++;
-	}
-	return (*b == '\0');
-}
-
-/**
  * _strstr - locates a substring.
  * @haystack: the string to be searched.
  * @needle: the substring being searched for.
@@ -33,11 +11,22 @@ int compare(char *a, char *b)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
+	int n;
+
+	if (*needle == 0)
 	{
-		if ((*haystack == *needle) && compare(haystack, needle))
+		return (haystack);
+	}
+	while (*haystack)
+	{
+		n = 0;
+		if (haystack[n] == needle[n])
 		{
-			return (haystack);
+			do {
+				if (needle[n + 1] == '\0')
+					return (haystack);
+				n++;
+			} while (haystack[n] == needle[n]);
 		}
 		haystack++;
 	}
