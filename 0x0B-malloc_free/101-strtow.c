5fe9c22d1463e7df223e260ae4e1fbb039306959
Laide Endeley <endeleylaide@gmail.com>
2023-05-15 07:55:14 -0700

    Updated 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,7 +46,7 @@ char **strtow(char *str)
 	w = wordCount(str);
 	if (w < 1)
 		return (NULL);
-	newstr = malloc(sizeof(char) * (w + 1));
+	newstr = malloc(sizeof(char *) * (w + 1));
 	if (newstr == NULL)
 		return (NULL);
 	i = 0;
@@ -57,7 +57,7 @@ char **strtow(char *str)
 			j = 0;
 			while (str[j] != ' ')
 				j++;
-			newstr[i] = malloc(sizeof(char) * (j + 1));
+			newstr[i] = malloc(sizeof(char) * j + 1);
 			if (newstr[i] == NULL)
 			{
 				while (--i >= 0)
