0996bee6f8dfd39fb2503f71389da559fb5fb8b0
Laide Endeley <endeleylaide@gmail.com>
2023-06-06 12:02:23 +0000

    Fixed output error in 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,6 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		c_node = c_node->next;
 	}
 	t_node = c_node->next;
+	c_node->next = t_node->next;
 	free(t_node);
 	return (1);
 }
